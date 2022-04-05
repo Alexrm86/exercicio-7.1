898c2d5d1ff076fc109f6fd5a7d5fd1ce63a76da
Alex Resende Machado <alex.magal@gmail.com>
2022-04-05 18:16:05 -0300

    1 arraw functions e letereres

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -12,18 +12,22 @@ const testingScope = escopo => {
 testingScope(true);
 
 
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// const sortOddsAndEvens = () => {
+//     oddsAndEvens[0] = 2;
+//     oddsAndEvens[1] = 3;
+//     oddsAndEvens[2] = 4;
+//     oddsAndEvens[3] = 7;
+//     oddsAndEvens[4] = 10;
+//     oddsAndEvens[5] = 13;
+
+//     return oddsAndEvens;
+// }
+
+// const sortedArray = sortOddsAndEvens();
+// console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const sortOddsAndEvens = () => {
-    oddsAndEvens[0] = 2;
-    oddsAndEvens[1] = 3;
-    oddsAndEvens[2] = 4;
-    oddsAndEvens[3] = 7;
-    oddsAndEvens[4] = 10;
-    oddsAndEvens[5] = 13;
-
-    return oddsAndEvens;
-}
-
-const sortedArray = sortOddsAndEvens();
-console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente !`);
